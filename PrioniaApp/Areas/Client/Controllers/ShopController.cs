f6f40a10a98f37a6027f75deb301c5229c0462f1
ravanmaxmud <ravanim@code.edu.az>
2023-01-24 21:42:19 +0400

    Aded Shop Individual Page

--- PrioniaApp/Areas/Client/Controllers/ShopController.cs
+++ PrioniaApp/Areas/Client/Controllers/ShopController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrioniaApp.Areas.Client.ViewModels.Shop;
+using PrioniaApp.Contracts.File;
+using PrioniaApp.Database;
+using PrioniaApp.Services.Abstracts;
 
 namespace PrioniaApp.Areas.Client.Controllers
 {
@@ -6,10 +11,46 @@ namespace PrioniaApp.Areas.Client.Controllers
     [Route("shop")]
     public class ShopController : Controller
     {
-        [HttpGet("index",Name ="client-shop-index")]
-        public IActionResult Index()
+
+        private readonly DataContext _dbContext;
+        private readonly IFileService _fileService;
+        public ShopController(DataContext dbContext, IFileService fileService)
         {
-            return View();
+            _dbContext = dbContext;
+            _fileService = fileService;
+        }
+
+
+        [HttpGet("index/{id}",Name ="client-shop-index")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var product = await _dbContext.Products.Include(p => p.ProductImages)
+                .Include(p => p.ProductColors)
+                .Include(p => p.ProductSizes).FirstOrDefaultAsync(p => p.Id == id);
+
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            var model = new ShopViewModel(product.Name, product.Description, product.Price,
+  
+                _dbContext.ProductColors.Include(pc => pc.Color).Where(pc => pc.ProductId == product.Id)
+                .Select(pc => new ShopViewModel.ColorViewModeL(pc.Color.Name, pc.Color.Id)).ToList(),
+
+                _dbContext.ProductSizes.Include(ps => ps.Size).Where(ps => ps.ProductId == product.Id)
+                .Select(ps => new ShopViewModel.SizeViewModeL(ps.Size.Title, ps.Size.Id)).ToList(),
+
+                _dbContext.ProductCatagories.Include(ps => ps.Catagory).Where(ps => ps.ProductId == product.Id)
+                .Select(ps => new ShopViewModel.CatagoryViewModeL(ps.Catagory.Title, ps.Catagory.Id)).ToList(),
+
+                  _dbContext.ProductTags.Include(ps => ps.Tag).Where(ps => ps.ProductId == product.Id)
+                .Select(ps => new ShopViewModel.TagViewModeL(ps.Tag.Title, ps.Tag.Id)).ToList(),
+
+                  _dbContext.ProductImages.Where(p=> p.ProductId == product.Id).Select(p=> new ShopViewModel.ImageViewModeL(_fileService.GetFileUrl(p.ImageNameInFileSystem,UploadDirectory.Products))).ToList()
+                );
+            return View(model);
         }
     }
 }
---
+++ PrioniaApp/Areas/Client/ViewModels/Shop/ShopViewModel.cs
@@ -0,0 +1,88 @@
+﻿namespace PrioniaApp.Areas.Client.ViewModels.Shop
+{
+    public class ShopViewModel
+    {
+        public ShopViewModel(string title, string description, int price, List<ColorViewModeL> colors, List<SizeViewModeL> sizes, List<CatagoryViewModeL> catagories, List<TagViewModeL> tags, List<ImageViewModeL> images)
+        {
+            Title = title;
+            Description = description;
+            Price = price;
+            Colors = colors;
+            Sizes = sizes;
+            Catagories = catagories;
+            Tags = tags;
+            Images = images;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public List<ColorViewModeL> Colors { get; set; }
+        public List<SizeViewModeL> Sizes { get; set; }
+        public List<CatagoryViewModeL> Catagories { get; set; }
+        public List<TagViewModeL> Tags { get; set; }
+        public List<ImageViewModeL> Images { get; set; }
+
+
+
+
+        public class ImageViewModeL
+        {
+            public ImageViewModeL(string imageUrl)
+            {
+                ImageUrl = imageUrl;
+            }
+            public string ImageUrl { get; set; }
+        }
+
+
+
+        public class CatagoryViewModeL
+        {
+            public CatagoryViewModeL(string title, int id)
+            {
+                Title = title;
+                Id = id;
+            }
+
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+        public class TagViewModeL
+        {
+            public TagViewModeL(string title, int id)
+            {
+                Title = title;
+                Id = id;
+            }
+
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+
+
+
+        public class SizeViewModeL
+        {
+            public SizeViewModeL(string title, int id)
+            {
+                Title = title;
+                Id = id;
+            }
+
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+        public class ColorViewModeL
+        {
+            public ColorViewModeL(string name, int id)
+            {
+                Name = name;
+                Id = id;
+            }
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
